d1f8ff47dad4f065bdb0aede4ae8c0ab2561b9f5
chaeyeong-im <immchae0@naver.com>
2024-12-09 00:12:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
 
-result=chat_model.invoke("안녕!")
-print(result.content)
-
+#subject="AI"
+#result = chat_model.invoke(subject + "에 대한 시를 써줘.")
+#print(result.content)
 
 import streamlit as st
-st.title("인공지능 시인")
+
+st.tilte("인공지능 시인")
+subject=st.text input("시의 주제를 입력해주세요.")
+st.write("시의 주제:" + subject)
+
+if st.button("시 작성:"):
+    with st.spinner("시 작성중..."):
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
+        st.write(result.content)
+        
